c7fad7a69908d534b1a24e7b01c284680ed21bd8
JeetShah <jeetsh4h@gmail.com>
2024-05-27 12:55:54 +0530

    REPL

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    if parse_cmd(&input) {
-        print!("Command found!");
-    } else {
-        print!("{}: command not found\n", input.trim_end());
+        if parse_cmd(&input) {
+            print!("Command found!\n");
+        } else {
+            print!("{}: command not found\n", input.trim_end());
+        }
     }
 }
 
