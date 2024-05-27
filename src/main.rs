6b50d90e4def3acc75fd6c3af65a9b22a0efe91c
JeetShah <jeetsh4h@gmail.com>
2024-05-27 12:45:42 +0530

    remove log line, add debug

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     print!("$ ");
     io::stdout().flush().unwrap();
